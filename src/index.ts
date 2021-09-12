0a74db56ee9828c64c12f5af8f2456fdf3400880
daftmaple <ubuntumaple@gmail.com>
2021-09-12 11:16:26 +1000

    Use allowlist to skip same user check

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-import {
-  AccessToken,
-  RefreshingAuthProvider,
-} from '@twurple/auth';
+import { AccessToken, RefreshingAuthProvider } from '@twurple/auth';
 import { ChatClient } from '@twurple/chat';
 
 const clientConfig = JSON.parse(
@@ -29,22 +26,22 @@ const tokenData = JSON.parse(
   fs.readFileSync(path.join(process.cwd(), '.config', 'tokens.json'), 'utf-8'),
 );
 
-let userlist: string[] = fs
+let userBanList: string[] = fs
   .readFileSync(path.join(process.cwd(), '.config', 'users.txt'), 'utf-8')
-  .split('\n');;
+  .split('\n');
 
-// Update every 60 minutes 
+// Update every 60 minutes
 const updateUserList = setInterval(() => {
-  userlist = fs
+  userBanList = fs
     .readFileSync(path.join(process.cwd(), '.config', 'users.txt'), 'utf-8')
     .split('\n');
-  console.log(`Loading ${userlist.length} users on banlist`);
+  console.log(`Loading ${userBanList.length} users on banlist`);
 }, 60 * 60 * 1000);
-
 
 const authProvider = new RefreshingAuthProvider(
   {
-    clientId, clientSecret,
+    clientId,
+    clientSecret,
     onRefresh: async (tokenData: AccessToken) => {
       fs.writeFileSync(
         path.join(process.cwd(), '.config', 'tokens.json'),
@@ -53,38 +50,44 @@ const authProvider = new RefreshingAuthProvider(
       );
     },
   },
-  tokenData
+  tokenData,
 );
 
-const chatClient = new ChatClient({authProvider, channels: botChannels});
+const chatClient = new ChatClient({ authProvider, channels: botChannels });
 
 chatClient.connect();
 chatClient.onConnect(() => {
   console.log(`Bot has been connected`);
   console.log(`Bot username: ${botName}`);
   console.log(`Bot version: ${botVersion}`);
-  console.log(`Loading ${userlist.length} users on banlist`);
+  console.log(`Loading ${userBanList.length} users on banlist`);
 });
 
 chatClient.onJoin((channel, user) => {
   console.log(`Joined channel ${channel}, user ${user}`);
 });
 
+const userAllowList: string[] = [];
+
 chatClient.onMessage((channel, user, _message, msg) => {
   // If user is broadcaster/mod/vip, skip filtering
   const isOkay =
-    msg.userInfo.isBroadcaster ||
-    msg.userInfo.isMod ||
-    msg.userInfo.isVip;
+    msg.userInfo.isBroadcaster || msg.userInfo.isMod || msg.userInfo.isVip;
   if (isOkay) return;
 
+  // Check if user has been checked previously
+  if (userAllowList.indexOf(user) !== -1) return;
+
   // Otherwise, check if listed
-  if (userlist.indexOf(user) !== -1) {
+  if (userBanList.indexOf(user) !== -1) {
     chatClient.ban(
       channel,
       user,
       'Your account is listed on banlist. If you are false negatives, please send an unban request in order to remove this username from banlist.',
     );
+  } else {
+    // If not in banlist, push to skip user check
+    userAllowList.push(user);
   }
 });
 
